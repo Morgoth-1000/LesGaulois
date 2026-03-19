c32939175171e6b74e2fc1643d9244825748661f
Margot Potevin <ptm5486a@ensfsi.onmicrosoft.com>
2026-03-19 11:28:10 +0100

    TP1 fini

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,33 +1,34 @@
 package personnages;
 
-public class Romain {
-	private String nom;
-	private int force;
-	
-	public Romain(String nom, int force) {
-		this.nom = nom;
-		this.force = force;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void parler(String texte) {
-		System.out.println(prendreParole() + "\"" + texte + "\"");
-	}
-	private String prendreParole() {
-		// TODO Auto-generated method stub
-		return "Le romain" + nom + ":";
-	}
+	public class Romain {
+		private String nom;
+		private int force;
 		
-	public void recevoirCoup(int forceCoup) {
-		force = force - forceCoup;
-		if (force < 1) {
-			force=0;
-			System.out.println("Le Romain " + nom + " : J'abandonne " );
+		public Romain(String nom, int force) {
+			this.nom = nom;
+			this.force = force;
 		}
-		else {
-			parler("Aie");
-		}		
-	
+		
+		public String getNom() {
+			return nom;
+		}
+		
+		public void parler(String texte) {
+			System.out.println(prendreParole() + "\"" + texte + "\"");
+		}
+		
+		private String prendreParole() {
+			return "Le Romain " + nom + " : ";
+		}
+		
+		public void recevoirCoup(int forceCoup) {
+			force -= forceCoup;
+			if (force < 1) {
+				force = 0;
+				parler("J'abandonne !");
+			} 
+			else {
+				parler("Aïe");
+			}
+		}
 	}
-}
---
+++ src/village_gaulois/Village.java
@@ -0,0 +1,18 @@
+package village_gaulois;
+
+public class Village {
+	private String nom;
+	private int nbVillageois = 0;
+	
+	public Village(String nom, int nbVillageois) {
+		this.nom = nom;
+		this.nbVillageois = nbVillageois;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public class Village {
+		public Gaulois
+	}
+
+}
